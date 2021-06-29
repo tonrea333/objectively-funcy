360e211e98bc5d11d8553c86a4761135071e0c51
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-29 10:33:11 -0400

    all

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
 // Your code here.
 
+function getFirstName(name) {//returns the first name of the given person
+  return name.firstName;
+}
 
+function getLastName(name) {//returns the last name of the given person
+  return name.lastName;
+}
+
+function getFullName(fullName) {//returns the first name and last name of the given person, with a space in the middle
+  firstandlastName = fullName.firstName.concat(" ", fullName.lastName);
+
+  return firstandlastName;
+}
+
+function setFirstName(name, nameChange) {
+  name.firstName = nameChange
+
+
+  return name.firstName;
+}
+
+function setAge(name, ageChange) {//sets age on the given person
+
+  name.age = ageChange
+  //console.log(name)
+  //console.log(name.age);
+  //console.log(name.ageChange)
+  //console.log(name.firstName);
+  //console.log(ageChange);
+  return name.ageChange;
+
+}
+function giveBirthday(name, baby) {
+  if (name) {
+    //console.log(name.age + 2)
+    //console.log(name)
+    return name.age + 1
+  } else if (baby) {
+    return baby[age + 1];
+  }
+}
+
+function marry(groom, bride, groom2, bride2){
+  let x = true//sets the marital status of both given people to true
+  groom.married = x;
+  bride.married = x;
+  //=========================================
+  console.log(groom)
+  console.log(bride)
+  bride.spouseName = "Colin Jaffe";
+  groom.spouseName = "Petra Solano"; 
+  bride2.spouseName = "Michael Cordero";
+  groom2.spouseName = "Jane Villanueva";
+}
 
 
 
